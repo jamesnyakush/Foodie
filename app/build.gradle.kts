50f87af6bedea688bde4853d80d79fda956dcde4
jamesnyakush <jnyakush99@gmail.com>
2023-08-19 12:28:12 +0300

    Migrated to Versions catalog :relaxed:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin)
 }
 
 android {
@@ -54,16 +55,16 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+
+    implementation(platform(libs.compose.bom.ui))
+    implementation(libs.bundles.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    androidTestImplementation(platform(libs.compose.bom.ui))
+
+    androidTestImplementation(libs.compose.ui.test)
+    debugImplementation(libs.bundles.compose.debug)
 }
